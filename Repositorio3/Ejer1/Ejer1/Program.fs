7d05cbb146b3794eab3171de04fa6faab9b02ded
OvidioTaleno <141462211+OvidioMartienezTaleno@users.noreply.github.com>
2023-09-16 03:25:24 -0600

    Add files via upload

---
+++ Repositorio3/Ejer1/Ejer1/Program.fs
@@ -0,0 +1,78 @@
+﻿open Microsoft.FSharp.Collections
+open Microsoft.FSharp.Core
+let rec agregar(lista : int list) n=
+    let mutable resul = []
+    if n <= 0 then
+        lista
+      
+    else
+        resul <- 0:: lista
+        agregar (resul)(n-1)
+
+let rec agregarIzq (lista : int list) n=
+    let mutable resul = []
+    if n <= 0 then
+        lista
+      
+    else
+        resul <- lista @ [0]
+        agregarIzq (resul)(n-1)
+
+        
+let desplazar (direccion : string, despla : int, listas : int list)=
+    let mutable newList = []
+    let cantidad = listas.Length
+    
+       
+    match direccion with
+    | "izq" ->
+        if despla >= cantidad then
+            let lis = agregar newList cantidad
+            printfn "Lista por la izquierda: %A, con despalzamiento de:  %d" lis despla
+            printfn "Original: %A" listas
+        
+        else
+            
+            let mutable con = despla+1; 
+            for e in listas do
+                if con <= cantidad then
+                    newList <- List.append newList [listas.Item(con-1)];
+                con <- con+1
+            
+            let lis = agregarIzq newList despla
+            printfn "Lista por la izquierda: %A,con despalazamiento de %d:" lis despla
+            printfn "Original: %A" listas
+            
+
+            
+   
+    | "der" ->
+        if despla < cantidad then 
+            let mutable cont = 0;
+            for e in listas do
+                if cont < (cantidad-despla) then
+                    newList <- List.append newList [e]
+                cont <- cont + 1
+                              
+                
+            let lis = agregar newList despla
+            printfn "Lista por la derecha: %A,con despalazamiento de %d:" lis despla
+            printfn "original: %A" listas
+        
+        else
+            let lis = agregar newList cantidad
+            printfn "Lista por la derecha: %A, con despalzamiento de:  %d" lis despla
+
+        
+    | _ ->
+        failwith "La dirección debe ser 'izq' o 'der'."
+
+let lista = [1;2;3;4;5]
+
+desplazar("izq" ,3, lista)
+printfn ""
+desplazar("der", 2, lista)
+printfn ""
+desplazar("izq", 6, lista)
+printfn ""
+desplazar("der", 4, lista)
---
+++ Repositorio3/Ejercicio2/Ejercicio2/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let rec filter (subcadena : string) (listaCadenas : string list) =
+    match listaCadenas with
+    | [] -> []  
+    | cabeza :: resto -> 
+        if cabeza.Contains(subcadena) then
+            cabeza :: filter subcadena resto  
+        else
+            filter subcadena resto  
+
+let sub_cadenas (subcadena : string) (listaCadenas : string list) =
+    let resultado = filter subcadena listaCadenas
+    resultado
+
+let listaOriginal = ["la casa"; "el perro"; "pintando la cerca"; "el mause"; "la funcion"]
+let subcadenaBuscada = "la"
+
+let resultadoFiltrado = sub_cadenas subcadenaBuscada listaOriginal
+
+printfn "Cadenas que contienen '%s': %A" subcadenaBuscada resultadoFiltrado
